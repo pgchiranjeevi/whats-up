effd5783233d60c878bf4bbe45672653b602eaf1
pgchiranjeevi <167151008+pgchiranjeevi@users.noreply.github.com>
2024-04-15 18:29:50 -0700

    Initial commit

---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student {
+	
+		
+	static int idcard = 12345;
+	 
+	String name = "Chiranjeevi";
+	
+	public static void chiran() {
+		
+		System.out.println("gggggggggggggg");
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(idcard);
+		
+		Student st = new Student();
+		System.out.println(st.name);
+		
+	int height = 100;
+	
+	System.out.println(height);
+System.out.println("welcome Chennai");
+
+
+	
+	
+	chiran();
+	
+		
+		}
+
+}
